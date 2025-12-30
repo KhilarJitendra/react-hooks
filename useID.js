472d833e036176acc0298a2facdf7d7ab0f077c8
Jitendra Khilar <97145002+KhilarJitendra@users.noreply.github.com>
2025-12-30 15:24:20 +0530

    useid hooks update

--- useID.js
+++ useID.js
@@ -6,6 +6,20 @@
 - We can use useId both in server as well client side
 
 
+IMP Concepts - 
+- I use useId for accessibility-related IDs like label, input, and ARIA attributes, especially in SSR. 
+  I avoid it for keys or business identifiers because it’s not stable or meaningful outside rendering.
+
+- without useId reusing the component would create duplicate IDs. useId guarantees unique, 
+  stable IDs per component instance, which is essential for accessibility and SSR.
+
+- ID is tied to the component instance, not each render.
+
+- useId generates deterministic IDs based on render order, not randomness. 
+  React guarantees the same order on the server and client, which makes the IDs stable and hydration-safe.
+
+
+
 import React, { useId } from 'react';
 
 export default function UseIdMultiple() {
