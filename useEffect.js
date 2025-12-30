fa89c15b7a656700047e6f8a7ab0bb8a7698c1db
Jitendra Khilar <97145002+KhilarJitendra@users.noreply.github.com>
2025-12-30 14:59:17 +0530

    Update useEffect.js

--- useEffect.js
+++ useEffect.js
@@ -3,7 +3,7 @@ useEffect
 - empty array - one time run
 - dependency changes - runs on dependency
 
-- Clean Up runs based on condition 
+- Clean Up runs based on condition  (when we unmount a component)
   - first time
   - dependency chnages - clean up runs first - use effects runs  
   - component unmount (removed from DOM like {isLoggedIn && <ComponentA> </ComponentA> }) then clean up runs
